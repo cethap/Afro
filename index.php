76ab7211e1f9401cf2eed337fbcb3e5fcc1dbdd0
James Brooks <james@bluebaytravel.co.uk>
2013-11-14 10:32:31 +0000

    Added some comments on the examples

--- index.php
+++ index.php
@@ -2,18 +2,31 @@
 
 	include "Afro.php";
 
+	/**
+	 * Both of these examples are simple route handling.
+	 * The first is an index page. Nothing exciting happening here.
+	 * And the second is another page, access via "/request".
+	 */
 	get('/', function($Afro) {
 		echo "Hello! From test.php";
 	});
-
 	get('/request', function($Afro) {
 		echo "<pre>".print_r($Afro, TRUE)."</pre>";
 	});
 
+	/**
+	 * An example of named parameters.
+	 * "blog" is not passed through as a parameter because it's part of the route.
+	 * In this example, both parameters must be passed through.
+	 */
 	get('/blog/(\w+)/(\d+)', function($Afro, $catID, $pageID) {
-		echo "Retrieving from blog: Category: {$catID} and Page ID: {$pageID}";
+		echo "Retrieving from Blog: Category: {$catID} and Page ID: {$pageID}";
 	});
 
+	/**
+	 * This route is setup so that parameters MUST be passed through, meaning you
+	 * cannot access /countries directly.
+	 */
 	get('/countries/(.*?)', function($Afro) {
 		$testData = array(
 			"mx" => array(
@@ -40,7 +53,7 @@
 				$hFile = fopen("php://output", "w");
 				fputcsv($hFile, array('ISO', 'Country'));
 
-				$lookingFor = $Afro->param(2);
+				$lookingFor = $Afro->param(1);
 				if(isset($testData[$lookingFor])) {
 					fputcsv($hFile, $testData[$lookingFor]);
 				}
@@ -52,14 +65,20 @@
 
 	});
 
-	get('/hello/(\w+)', function($Afro, $userName) {
-		$Afro->format('json', function($Afro) use ($userName) {
-			echo json_encode(array('name', $userName));
+	/**
+	 * This example uses the old way of retrieving parameters, using the param object.
+	 * It's still a valid and useful way of doing things, but without named parameters.
+	 * Notice that the "name" parameter now lies in index 1, previously that would be 2,
+	 * because "hello" is the first parameter.
+	 * The first argument that isn't dynamic is now removed.
+	 */
+	get('/hello/(.*?)', function($Afro) {
+		$Afro->format('json', function($Afro) {
+			echo json_encode(array('name', $Afro->param(1)));
 		});
 
-		if(!$Afro->format) {
-			echo 'Hello '. $userName . ', it\'s a good day today!';
-		}
+		if(!$Afro->format)
+			echo 'Hello '. $Afro->param(1) . ', it\'s a good day today!';
 	});
 
 ?>
